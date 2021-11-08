f04992e9fec268b49fdc07575b4c7585989b8b29
Christophverstraeten <chrv@live.be>
2021-11-08 19:45:44 +0100

    Assigns houdt nu enkel nog de meest recente waarde bij

--- programConverter.py
+++ programConverter.py
@@ -11,18 +11,14 @@ class AssignmentVisitor(ast.NodeVisitor):
     def visit_Assign(self, node):
         for target in node.targets:
             if isinstance(target, ast.Name) & isinstance(node.value, ast.Constant):
-                if target.id not in assigns:
-                    assigns[target.id] = [node.value.value]
-                else:
-                    assigns.get(target.id).insert(0, node.value.value)
+                assigns[target.id] = node.value.value
 
     def visit_Add(self, node):
-        print(assigns.get(self.leftOperand)[0] + assigns.get(self.rightOperand)[0])
+        assigns.get(self.leftOperand) + assigns.get(self.rightOperand)
 
     def visit_AugAssign(self, node):
         if isinstance(node.value, ast.Name) & isinstance(node.target, ast.Name):
-            assigns.get(node.target.id).insert(0, assigns.get(node.target.id)[0] + assigns.get(node.value.id)[0])
-
+            assigns[node.target.id] = assigns.get(node.target.id) + assigns.get(node.value.id)
     def visit_BinOp(self, node):
         if isinstance(node.left, ast.Name) & isinstance(node.right, ast.Name):
             self.leftOperand = node.left.id
@@ -38,10 +34,10 @@ class AssignmentVisitor(ast.NodeVisitor):
 def main(text):
     usage_analyzer = AssignmentVisitor()
     tree = ast.parse(text)
-    print(ast.dump(tree))
+    #print(ast.dump(tree))
     usage_analyzer.visit(tree)
     print(assigns)
 
 
 if __name__ == '__main__':
-    main("""a = 1\nb = 9\nc = 4\na = 3\na + c\na += c\n""")
+    main("""a = 1\nb = 9\nc = 4\na = 3\na + c\na += c\nb = a + c\n""")
