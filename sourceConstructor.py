89967648d484d1f6cbbabd4577848a2b6ffbbf0a
Seppe Lesschaeve <lesschaeveseppe@gmail.com>
2021-11-13 20:27:02 +0100

    Code cleaned, ready to review!

--- sourceConstructor.py
+++ sourceConstructor.py
@@ -10,10 +10,16 @@ class SourceVisitor(ast.NodeVisitor):
         self.referencePool = {}
 
     def set_left_operand(self, left_operand):
-        self.left_operand = left_operand
+        if left_operand in self.source:
+            self.left_operand = self.source[left_operand]
+        else:
+            self.left_operand = left_operand
 
     def set_right_operand(self, right_operand):
-        self.right_operand = right_operand
+        if right_operand in self.source:
+            self.right_operand = self.source[right_operand]
+        else:
+            self.right_operand = right_operand
 
     def set_operands(self, left_operand, right_operand):
         self.set_left_operand(left_operand)
@@ -23,26 +29,13 @@ class SourceVisitor(ast.NodeVisitor):
         return node.value
 
     def visit_Name(self, node):
-        return self.source[node.id]
+        return node.id
 
     def visit_Assign(self, node):
         for target in node.targets:
-            if isinstance(target, ast.Name):
-                if isinstance(node.value, ast.Constant):
-                    self.source[target.id] = node.value.value
-                elif isinstance(node.value, ast.BinOp):
-                    self.set_operands(self.evaluate_computation(node.value.left),
-                                      self.evaluate_computation(node.value.right))
-                    self.source[target.id] = self.visit(node.value.op)
-                elif isinstance(node.value, ast.List):
-                    self.source[target.id] = []
-                    for element in node.value.elts:
-                        if isinstance(element, ast.Constant):
-                            self.source[target.id].append(element.value)
-                elif isinstance(node.value, ast.Call):
-                    self.source[target.id] = self.visit_Call(node.value)
-                if target.id in self.referencePool:
-                    self.updateAll(target.id)
+            self.source[self.visit(target)] = self.visit(node.value)
+            if self.visit(target) in self.referencePool:
+                self.updateAll(self.visit(target))
                 print(self.source)
 
     def visit_Add(self, node):
@@ -58,61 +51,25 @@ class SourceVisitor(ast.NodeVisitor):
         return self.left_operand / self.right_operand
 
     def visit_AugAssign(self, node):
-        evaluation = self.evaluate_computation(node.value)
-        result = self.perform_computation(node.target, node.op, evaluation)
-        if isinstance(node.target, ast.Name):
-            self.source[node.target.id] = result
-            self.updateAll(node.target.id)
-        if isinstance(node.target, ast.Subscript):
-            if isinstance(node.target.slice, ast.Index):
-                if isinstance(node.target.value, ast.Name):
-                    if isinstance(node.target.slice.value, ast.Constant):
-                        self.source[node.target.value.id][node.target.slice.value.value] = result
-                    if isinstance(node.target.slice.value, ast.Name):
-                        self.source[node.target.value.id][self.visit(node.target.slice.value)] = result
-                    self.updateAll(node.target.value.id)
+        self.set_operands(self.visit(node.target), self.visit(node.value))
+        if self.visit(node.target) in self.source:
+            self.source[self.visit(node.target)] = self.visit(node.op)
+            self.updateAll(self.visit(node.target))
+        elif isinstance(node.target, ast.Subscript):
+            self.source[self.visit(node.target.value)][self.visit(node.target.slice)] = self.visit(node.op)
+            self.updateAll(self.visit(node.target.value))
         print(self.source)
 
-    def perform_computation(self, target, op, evaluation):
-        if isinstance(target, ast.Name):
-            return self.perform_computation(self.source[target.id], op, evaluation)
-        if isinstance(target, ast.Subscript):
-            if isinstance(target.slice, ast.Index):
-                if isinstance(target.value, ast.Name):
-                    if isinstance(target.slice.value, ast.Constant):
-                        return self.perform_computation(self.source[target.value.id][target.slice.value.value],
-                                                        op, evaluation)
-                    if isinstance(target.slice.value, ast.Name):
-                        return self.perform_computation(self.source[target.value.id][self.visit(target.slice.value)],
-                                                        op, evaluation)
-        self.set_operands(target, evaluation)
-        return self.visit(op)
+    def visit_Subscript(self, node):
+        return self.source[self.visit(node.value)][self.visit(node.slice)]
 
-    def evaluate_computation(self, value):
-        if isinstance(value, ast.Constant):
-            return value.value
-        if isinstance(value, ast.Name):
-            return self.source[value.id]
-        if isinstance(value, ast.BinOp):
-            self.set_operands(self.evaluate_computation(value.left), self.evaluate_computation(value.right))
-            return self.visit(value.op)
-        if isinstance(value, ast.Subscript):
-            if isinstance(value.slice, ast.Index):
-                if isinstance(value.value, ast.Name):
-                    if isinstance(value.slice.value, ast.Constant):
-                        return self.source[value.value.id][value.slice.value.value]
-                    if isinstance(value.slice.value, ast.Name):
-                        return self.source[value.value.id][self.visit(value.slice.value)]
-
-
+    def visit_Index(self, node):
+        return self.source[self.visit(node.value)]
 
     def visit_Compare(self, node):
-        self.left_operand = self.evaluate_computation(node.left)
+        self.set_left_operand(self.visit(node.left))
         for c in node.comparators:
-            if isinstance(c, ast.Constant):
-                self.right_operand = c.value
-            if isinstance(c, ast.Name):
-                self.right_operand = self.source[c.id]
+            self.set_right_operand(self.visit(c))
         for o in node.ops:
             return self.visit(o)
 
@@ -134,25 +91,34 @@ class SourceVisitor(ast.NodeVisitor):
     def visit_GtE(self, node):
         return self.left_operand >= self.right_operand
 
+    def visit_List(self, node):
+        elements = []
+        for element in node.elts:
+            elements.append(self.visit(element))
+        return elements
+
+    def visit_In(self, node):
+        return self.left_operand in self.right_operand
+
     def visit_Expr(self, node):
-        if isinstance(node.value, ast.BinOp):
-            self.visit_BinOp(node.value)
+        self.visit(node.value)
+
+    def visit_BinOp(self, node):
+        self.set_operands(self.visit(node.left), self.visit(node.right))
+        return self.visit(node.op)
 
     def visit_If(self, node):
-        if isinstance(node.test, ast.BoolOp):
-            if self.visit_BoolOp(node.test):
-                for n in node.body:
-                    self.visit(n)
-            elif node.orelse:
-                for n in node.orelse:
-                    self.visit(n)
+        if self.visit(node.test):
+            for n in node.body:
+                self.visit(n)
+        elif node.orelse:
+            for n in node.orelse:
+                self.visit(n)
 
     def visit_BoolOp(self, node):
         if len(node.values) == 2:
-            left_operand = self.visit(node.values[0])
-            right_operand = self.visit(node.values[1])
-        self.set_operands(left_operand, right_operand)
-        return self.visit(node.op)
+            self.set_operands(self.visit(node.values[0]), self.visit(node.values[1]))
+            return self.visit(node.op)
 
     def visit_And(self, node):
         return self.left_operand and self.right_operand
@@ -189,8 +155,7 @@ class SourceVisitor(ast.NodeVisitor):
             self.visit(statement)
         for key in tempSource:
             if len(tempSource[key]) == 2:
-                if len(tempSource[key][1]) == 2:
-                    ts[tempSource[key][1][0]] = self.source[key]
+                ts[tempSource[key][1][0]] = self.source[key]
         self.source = ts
 
     def is_immutable(self, obj):
@@ -215,8 +180,6 @@ class SourceVisitor(ast.NodeVisitor):
                     self.visit(n)
             self.source.pop(node.target.id)
 
-
-
     def visit_Expr(self, node):
         if isinstance(node.value, ast.Call):
             self.visit_Call(node.value)
@@ -224,37 +187,36 @@ class SourceVisitor(ast.NodeVisitor):
     def visit_Builtin(self, node):
         if isinstance(node.func, ast.Attribute):
             if node.func.attr == 'append':
-                self.source[node.func.value.id] = self.visit_append(self.visit(node.func.value), self.visit(node.args[0]))
-                self.updateAll(node.func.value.id)
-                return self.source[node.func.value.id]
+                self.set_operands(self.visit(node.func.value), self.visit(node.args[0]))
+                self.source[self.visit(node.func.value)] = self.visit_append()
+                self.updateAll(self.visit(node.func.value))
         if isinstance(node.func, ast.Name):
             if node.func.id == 'len':
                 return len(self.source[node.args[0].id])
             if node.func.id == 'range':
                 return [self.visit(node.args[0]), self.visit(node.args[1])]
 
-    def visit_append(self, array, element):
-        return array + [element]
+    def visit_append(self):
+        return self.left_operand + [self.right_operand]
 
     def buildTempSource(self, node):
         tempSource = {}
         if isinstance(node.args, list):
             for i in range(0, len(node.args)):
-                if isinstance(node.args[i], ast.Name):
-                    tempArg = node.args[i].id
-                node.args[i] = self.visit(node.args[i])
-                if self.is_immutable(node.args[i]):
-                    tempSource[self.source[node.func.id][0][i]] = [[node.args[i]]]
+                tempArg = self.visit(node.args[i])
+                if self.is_immutable(self.source[self.visit(node.args[i])]):
+                    tempSource[self.source[node.func.id][0][i]] = [[self.source[self.visit(node.args[i])]]]
                 else:
-                    tempSource[self.source[node.func.id][0][i]] = [[node.args[i]], [tempArg]]
+                    tempSource[self.source[node.func.id][0][i]] = [[self.source[self.visit(node.args[i])]], [tempArg]]
         for j in range(0, len(node.keywords)):
-            if isinstance(node.keywords[j].value, ast.Name):
-                tempId = node.keywords[j].value.id
-            node.keywords[j].value = self.visit(node.keywords[j].value)
-            if self.is_immutable(node.keywords[j].value):
-                tempSource[node.keywords[j].arg] = [[node.keywords[j].value]]
+            if isinstance(node.keywords[j].value, ast.Constant):
+                tempSource[node.keywords[j].arg] = [[self.visit(node.keywords[j].value)]]
             else:
-                tempSource[node.keywords[j].arg] = [[node.keywords[j].value], [tempId]]
+                tempId = self.visit(node.keywords[j].value)
+                if self.is_immutable(self.source[self.visit(node.keywords[j].value)]):
+                    tempSource[node.keywords[j].arg] = [[self.source[self.visit(node.keywords[j].value)]]]
+                else:
+                    tempSource[node.keywords[j].arg] = [[self.source[self.visit(node.keywords[j].value)]], [tempId]]
         return tempSource
 
     def updateAll(self, updated_reference):
@@ -290,6 +252,8 @@ if __name__ == '__main__':
     b += 1
     c = 1 + b 
     l.append(c)
+    if 1 in l:
+        l.append(1)
     d = range(0, len(l))
     
     
