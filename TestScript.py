e3e72289a83e1ec743110b4ffb5b7e2ab90e8928
Christoph Verstraeten <christoph.verstraeten@student.kuleuven.be>
2022-02-02 04:00:32 +0100

    put old test in Testscript

--- TestScript.py
+++ TestScript.py
@@ -1,17 +1,41 @@
 def test(a, b, l):
     a.append(1)
     b += 1
-    c = 1 + b
+    c = 1 + b 
     l.append(c)
-    print(a)
-    print(b)
-    print(ll)
-
-
+    l.pop()
+    l.sort()
+    if 1 in l:
+        l.append(1)
+    d = range(0, len(l))
+    
+    
 a = 2
 b = 4
 ll = [1, 2, 3, 4]
-
-test(ll, 1, ll)
-print(a)
-print(ll)
+if (a * 2 >= 4) and (1 > a or b > 3):
+    c = 2
+    while c > 0:
+        c -= 1
+else:
+    for i in range(0, len(ll)):
+        ll[i] *= ll[i]
+    for i in ll:
+        i /= i
+    else:
+        b += 1
+if (a * 2 >= 4) and (1 > a or b > 3):
+    for i in range(0, len(ll)):
+        ll[i] *= ll[i]
+    for i in ll:
+        i = 2
+    else:
+        b += 1
+else:
+    c = 2
+    while c > 0:
+        c -= 1
+a = 4
+b += a
+test(ll, l = ll, b = 1)
+print(a,b,c,i,ll)
