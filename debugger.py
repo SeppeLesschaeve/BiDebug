235a6ed489417a9a31a6dc400b5ffd2d53b42bc2
Seppe Lesschaeve <lesschaeveseppe@gmail.com>
2022-02-24 17:23:01 +0100

    Some progress in creating the structure of a program

--- debugger.py
+++ debugger.py
@@ -12,9 +12,8 @@ from typing import Any
 
 class Operation:
 
-    def __init__(self, parent_operation):
-        self.parent_operation = parent_operation
-        self.operation = None
+    def __init__(self):
+        self.parent_operation = None
 
     def update_next(self):
         return
@@ -25,16 +24,16 @@ class Operation:
 
 class CompositeOperation(Operation):
 
-    def __init__(self, parent_operation):
-        Operation.__init__(self, parent_operation)
+    def __init__(self):
+        Operation.__init__(self)
         self.operations = None
 
 
 class IfThenElseOperation(CompositeOperation):
 
-    def __init__(self, parent_operation):
+    def __init__(self):
         self.choices_stack = []
-        CompositeOperation.__init__(self, parent_operation)
+        CompositeOperation.__init__(self)
 
     def update_previous(self):
         self.choices_stack.pop()
@@ -42,9 +41,9 @@ class IfThenElseOperation(CompositeOperation):
 
 class WhileOperation(CompositeOperation):
 
-    def __init__(self, parent_operation):
+    def __init__(self):
         self.number = 0
-        CompositeOperation.__init__(self, parent_operation)
+        CompositeOperation.__init__(self)
 
     def update_next(self):
         self.number += 1
@@ -55,16 +54,23 @@ class WhileOperation(CompositeOperation):
 
 class ForOperation(CompositeOperation):
 
-    def __init__(self, parent_operation):
+    def __init__(self):
         self.index = 0
         self.collection = []
-        Operation.__init__(self, parent_operation)
+        CompositeOperation.__init__(self)
 
     def update_next(self):
         self.index += 1
 
     def update_previous(self):
         self.index -= 1
+
+
+class FunctionOperation(CompositeOperation):
+
+    def __init__(self):
+        self.arguments = None
+        CompositeOperation.__init__(self)
 
 
 class ReverseVisitor(ast.NodeVisitor):
@@ -731,334 +737,374 @@ class ForwardVisitor(ast.NodeVisitor):
 
 class SourceCreator(ast.NodeVisitor):
 
-    def visit(self, node: AST) -> Any:
-        return super().visit(node)
-
-    def generic_visit(self, node: AST) -> Any:
-        return super().generic_visit(node)
-
     def visit_Module(self, node: Module) -> Any:
-        return super().visit_Module(node)
-
-    def visit_Interactive(self, node: Interactive) -> Any:
-        return super().visit_Interactive(node)
-
-    def visit_Expression(self, node: Expression) -> Any:
-        return super().visit_Expression(node)
+        for program in node.body:
+            self.visit(program)
 
     def visit_FunctionDef(self, node: FunctionDef) -> Any:
-        return super().visit_FunctionDef(node)
+        function_def = FunctionOperation()
+        function_def.name = node.name
+        args = {}
+        for argument in node.args.args:
+            args[argument.arg] = []
+        function_def.arguments = args
+        operations = []
+        for statement in node.body:
+            operations.append(self.visit(statement))
+        for operation in operations:
+            operation.parent_operation = function_def
+        function_def.operations = operations
+        self.source[node.name] = function_def
 
     def visit_AsyncFunctionDef(self, node: AsyncFunctionDef) -> Any:
-        return super().visit_AsyncFunctionDef(node)
+        return node
 
     def visit_ClassDef(self, node: ClassDef) -> Any:
-        return super().visit_ClassDef(node)
+        return node
 
     def visit_Return(self, node: Return) -> Any:
-        return super().visit_Return(node)
+        return node
 
     def visit_Delete(self, node: Delete) -> Any:
-        return super().visit_Delete(node)
+        return node
 
     def visit_Assign(self, node: Assign) -> Any:
-        return super().visit_Assign(node)
+        return node
 
     def visit_AugAssign(self, node: AugAssign) -> Any:
-        return super().visit_AugAssign(node)
+        return node
 
     def visit_AnnAssign(self, node: AnnAssign) -> Any:
-        return super().visit_AnnAssign(node)
+        return node
 
     def visit_For(self, node: For) -> Any:
-        return super().visit_For(node)
+        for_operation = ForOperation()
+        for_operation.target = node.target
+        for_operation.iter = node.iter
+        operations = []
+        for statement in node.body:
+            operations.append(self.visit(statement))
+        for operation in operations:
+            operation.parent_operation = for_operation
+        for_operation.operations = operations
+        return for_operation
 
     def visit_AsyncFor(self, node: AsyncFor) -> Any:
-        return super().visit_AsyncFor(node)
+        return node
 
     def visit_While(self, node: While) -> Any:
-        return super().visit_While(node)
+        while_operation = WhileOperation()
+        operations = [node.test]
+        for statement in node.body:
+            operations.append(self.visit(statement))
+        for operation in operations:
+            operation.parent_operation = while_operation
+        while_operation.operations = operations
+        return while_operation
 
     def visit_If(self, node: If) -> Any:
-        return super().visit_If(node)
+        if_then_else_operation = IfThenElseOperation()
+        operations = [node.test]
+        then_operations = []
+        for statement in node.body:
+            then_operations.append(self.visit(statement))
+        for operation in then_operations:
+            operation.parent_operation = if_then_else_operation
+        else_operations = []
+        for statement in node.orelse:
+            else_operations.append(self.visit(statement))
+        for operation in else_operations:
+            operation.parent_operation = if_then_else_operation
+        for operation in operations:
+            operation.parent_operation = if_then_else_operation
+        operations.append(then_operations)
+        operations.append(else_operations)
+        if_then_else_operation.operations = operations
+        return if_then_else_operation
 
     def visit_With(self, node: With) -> Any:
-        return super().visit_With(node)
+        return node
 
     def visit_AsyncWith(self, node: AsyncWith) -> Any:
-        return super().visit_AsyncWith(node)
+        return node
 
     def visit_Raise(self, node: Raise) -> Any:
-        return super().visit_Raise(node)
+        return node
 
     def visit_Try(self, node: Try) -> Any:
-        return super().visit_Try(node)
+        return node
 
     def visit_Assert(self, node: Assert) -> Any:
-        return super().visit_Assert(node)
+        return node
 
     def visit_Import(self, node: Import) -> Any:
-        return super().visit_Import(node)
+        return node
 
     def visit_ImportFrom(self, node: ImportFrom) -> Any:
-        return super().visit_ImportFrom(node)
+        return node
 
     def visit_Global(self, node: Global) -> Any:
-        return super().visit_Global(node)
+        return node
 
     def visit_Nonlocal(self, node: Nonlocal) -> Any:
-        return super().visit_Nonlocal(node)
+        return node
 
     def visit_Expr(self, node: Expr) -> Any:
-        return super().visit_Expr(node)
+        return node
 
     def visit_Pass(self, node: Pass) -> Any:
-        return super().visit_Pass(node)
+        return node
 
     def visit_Break(self, node: Break) -> Any:
-        return super().visit_Break(node)
+        return node
 
     def visit_Continue(self, node: Continue) -> Any:
-        return super().visit_Continue(node)
+        return node
 
     def visit_Slice(self, node: Slice) -> Any:
-        return super().visit_Slice(node)
+        return node
 
     def visit_BoolOp(self, node: BoolOp) -> Any:
-        return super().visit_BoolOp(node)
+        return node
 
     def visit_BinOp(self, node: BinOp) -> Any:
-        return super().visit_BinOp(node)
+        return node
 
     def visit_UnaryOp(self, node: UnaryOp) -> Any:
-        return super().visit_UnaryOp(node)
+        return node
 
     def visit_Lambda(self, node: Lambda) -> Any:
-        return super().visit_Lambda(node)
+        return node
 
     def visit_IfExp(self, node: IfExp) -> Any:
-        return super().visit_IfExp(node)
+        if_then_else_operation = IfThenElseOperation()
+        operations = [node.test, self.visit(node.body), self.visit(node.orelse)]
+        for operation in operations:
+            operation.parent_operation = if_then_else_operation
+        if_then_else_operation.operations = operations
+        return if_then_else_operation
 
     def visit_Dict(self, node: Dict) -> Any:
-        return super().visit_Dict(node)
+        return node
 
     def visit_Set(self, node: Set) -> Any:
-        return super().visit_Set(node)
+        return node
 
     def visit_ListComp(self, node: ListComp) -> Any:
-        return super().visit_ListComp(node)
+        return node
 
     def visit_SetComp(self, node: SetComp) -> Any:
-        return super().visit_SetComp(node)
+        return node
 
     def visit_DictComp(self, node: DictComp) -> Any:
-        return super().visit_DictComp(node)
+        return node
 
     def visit_GeneratorExp(self, node: GeneratorExp) -> Any:
-        return super().visit_GeneratorExp(node)
+        return node
 
     def visit_Await(self, node: Await) -> Any:
-        return super().visit_Await(node)
+        return node
 
     def visit_Yield(self, node: Yield) -> Any:
-        return super().visit_Yield(node)
+        return node
 
     def visit_YieldFrom(self, node: YieldFrom) -> Any:
-        return super().visit_YieldFrom(node)
+        return node
 
     def visit_Compare(self, node: Compare) -> Any:
-        return super().visit_Compare(node)
+        return node
 
     def visit_Call(self, node: Call) -> Any:
-        return super().visit_Call(node)
+        return node
 
     def visit_FormattedValue(self, node: FormattedValue) -> Any:
-        return super().visit_FormattedValue(node)
+        return node
 
     def visit_JoinedStr(self, node: JoinedStr) -> Any:
-        return super().visit_JoinedStr(node)
+        return node
 
     def visit_Constant(self, node: Constant) -> Any:
-        return super().visit_Constant(node)
+        return node
 
     def visit_NamedExpr(self, node: NamedExpr) -> Any:
-        return super().visit_NamedExpr(node)
+        return node
 
     def visit_Attribute(self, node: Attribute) -> Any:
-        return super().visit_Attribute(node)
+        return node
 
     def visit_Subscript(self, node: Subscript) -> Any:
-        return super().visit_Subscript(node)
+        return node
 
     def visit_Starred(self, node: Starred) -> Any:
-        return super().visit_Starred(node)
+        return node
 
     def visit_Name(self, node: Name) -> Any:
-        return super().visit_Name(node)
+        return node
 
     def visit_List(self, node: List) -> Any:
-        return super().visit_List(node)
+        return node
 
     def visit_Tuple(self, node: Tuple) -> Any:
-        return super().visit_Tuple(node)
+        return node
 
     def visit_Del(self, node: Del) -> Any:
-        return super().visit_Del(node)
+        return node
 
     def visit_Load(self, node: Load) -> Any:
-        return super().visit_Load(node)
+        return node
 
     def visit_Store(self, node: Store) -> Any:
-        return super().visit_Store(node)
+        return node
 
     def visit_And(self, node: And) -> Any:
-        return super().visit_And(node)
+        return node
 
     def visit_Or(self, node: Or) -> Any:
-        return super().visit_Or(node)
+        return node
 
     def visit_Add(self, node: Add) -> Any:
-        return super().visit_Add(node)
+        return node
 
     def visit_BitAnd(self, node: BitAnd) -> Any:
-        return super().visit_BitAnd(node)
+        return node
 
     def visit_BitOr(self, node: BitOr) -> Any:
-        return super().visit_BitOr(node)
+        return node
 
     def visit_BitXor(self, node: BitXor) -> Any:
-        return super().visit_BitXor(node)
+        return node
 
     def visit_Div(self, node: Div) -> Any:
-        return super().visit_Div(node)
+        return node
 
     def visit_FloorDiv(self, node: FloorDiv) -> Any:
-        return super().visit_FloorDiv(node)
+        return node
 
     def visit_LShift(self, node: LShift) -> Any:
-        return super().visit_LShift(node)
+        return node
 
     def visit_Mod(self, node: Mod) -> Any:
-        return super().visit_Mod(node)
+        return node
 
     def visit_Mult(self, node: Mult) -> Any:
-        return super().visit_Mult(node)
+        return node
 
     def visit_MatMult(self, node: MatMult) -> Any:
-        return super().visit_MatMult(node)
+        return node
 
     def visit_Pow(self, node: Pow) -> Any:
-        return super().visit_Pow(node)
+        return node
 
     def visit_RShift(self, node: RShift) -> Any:
-        return super().visit_RShift(node)
+        return node
 
     def visit_Sub(self, node: Sub) -> Any:
-        return super().visit_Sub(node)
+        return node
 
     def visit_Invert(self, node: Invert) -> Any:
-        return super().visit_Invert(node)
+        return node
 
     def visit_Not(self, node: Not) -> Any:
-        return super().visit_Not(node)
+        return node
 
     def visit_UAdd(self, node: UAdd) -> Any:
-        return super().visit_UAdd(node)
+        return node
 
     def visit_USub(self, node: USub) -> Any:
-        return super().visit_USub(node)
+        return node
 
     def visit_Eq(self, node: Eq) -> Any:
-        return super().visit_Eq(node)
+        return node
 
     def visit_Gt(self, node: Gt) -> Any:
-        return super().visit_Gt(node)
+        return node
 
     def visit_GtE(self, node: GtE) -> Any:
-        return super().visit_GtE(node)
+        return node
 
     def visit_In(self, node: In) -> Any:
-        return super().visit_In(node)
+        return node
 
     def visit_Is(self, node: Is) -> Any:
-        return super().visit_Is(node)
+        return node
 
     def visit_IsNot(self, node: IsNot) -> Any:
-        return super().visit_IsNot(node)
+        return node
 
     def visit_Lt(self, node: Lt) -> Any:
-        return super().visit_Lt(node)
+        return node
 
     def visit_LtE(self, node: LtE) -> Any:
-        return super().visit_LtE(node)
+        return node
 
     def visit_NotEq(self, node: NotEq) -> Any:
-        return super().visit_NotEq(node)
+        return node
 
     def visit_NotIn(self, node: NotIn) -> Any:
-        return super().visit_NotIn(node)
+        return node
 
     def visit_comprehension(self, node: comprehension) -> Any:
-        return super().visit_comprehension(node)
+        return node
 
     def visit_ExceptHandler(self, node: ExceptHandler) -> Any:
-        return super().visit_ExceptHandler(node)
+        return node
 
     def visit_arguments(self, node: arguments) -> Any:
-        return super().visit_arguments(node)
+        return node
 
     def visit_arg(self, node: arg) -> Any:
-        return super().visit_arg(node)
+        return node
 
     def visit_keyword(self, node: keyword) -> Any:
-        return super().visit_keyword(node)
+        return node
 
     def visit_alias(self, node: alias) -> Any:
-        return super().visit_alias(node)
+        return node
 
     def visit_withitem(self, node: withitem) -> Any:
-        return super().visit_withitem(node)
+        return node
 
     def visit_ExtSlice(self, node: ExtSlice) -> Any:
-        return super().visit_ExtSlice(node)
+        return node
 
     def visit_Index(self, node: Index) -> Any:
-        return super().visit_Index(node)
+        return node
 
     def visit_Suite(self, node: Suite) -> Any:
-        return super().visit_Suite(node)
+        return node
 
     def visit_AugLoad(self, node: AugLoad) -> Any:
-        return super().visit_AugLoad(node)
+        return node
 
     def visit_AugStore(self, node: AugStore) -> Any:
-        return super().visit_AugStore(node)
+        return node
 
     def visit_Param(self, node: Param) -> Any:
-        return super().visit_Param(node)
+        return node
 
     def visit_Num(self, node: Num) -> Any:
-        return super().visit_Num(node)
+        return node
 
     def visit_Str(self, node: Str) -> Any:
-        return super().visit_Str(node)
+        return node
 
     def visit_Bytes(self, node: Bytes) -> Any:
-        return super().visit_Bytes(node)
+        return node
 
     def visit_NameConstant(self, node: NameConstant) -> Any:
-        return super().visit_NameConstant(node)
+        return node
 
     def visit_Ellipsis(self, node: Ellipsis) -> Any:
-        return super().visit_Ellipsis(node)
+        return node
 
     def __init__(self):
         self.tree = None
-        self.source = None
+        self.source = {}
 
     def build_tree(self, text):
         self.tree = ast.parse(text)
         self.visit(self.tree)
+        print(self.source)
 
     def update(self, source):
         return
@@ -1091,3 +1137,26 @@ class Debugger:
 
     def execute(self, number):
         self.source_controller.execute(number)
+
+
+def main(source):
+    # print(ast.dump(tree))
+    source_constructor = SourceCreator()
+    source_constructor.build_tree(source)
+    # print(usage_analyzer.source)
+
+
+if __name__ == '__main__':
+    input_program = """
+def test(a, b, l):
+    b += 1
+    c = 1 + b 
+    while b > 1:
+        for x in l:
+            if x < 2:
+                x = 2
+            else:
+                x *= 2
+
+"""
+    main(input_program)
