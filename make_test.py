93f0e91cc71ff072dcfef98ca5a4ad1727d495a9
Christoph Verstraeten <christoph.verstraeten@student.kuleuven.be>
2022-05-30 00:54:33 +0200

    added test for reverse execution of while loop

--- make_test.py
+++ make_test.py
@@ -2,17 +2,18 @@ from operator import mod
 
 
 if __name__ == "__main__":
-    amount = 10000
+    amount = 500
     f = open("proef_statement.py",mode="w+")
     f.truncate(0)
+    f.write("i = 1\n")
     for i in range(amount):
-        f.write("if True:\n\t1 + 1\n")
+        f.write("while i == 1:\n\ti -= 1\nwhile i == 0:\n\ti += 1\n")
     f.close()
     f = open("infile.txt",mode="w+")
     f.truncate(0)
     for i in range(amount):
         f.write("1\n")
-    #for i in range(amount):
-        #f.write("2\n")
+    for i in range(amount):
+        f.write("2\n")
     f.write("3\n")
     f.close()
